f685426144b2aa3dcbfd72fa92fdda5801a88c98
Pilar-B <pbarkerit@gmail.com>
2021-09-24 16:43:41 -0400

    test change

--- js/scripts.js
+++ js/scripts.js
@@ -17,8 +17,8 @@ jQuery(document).ready(function () {
     // });
 
     $("p").click(function () {
-        $(".walrus-showing").show();
-        $(".walrus-hidden").hide();
+        $(".walrus-showing").toggle();
+        $(".walrus-hidden").toggle();
 
     });
 
