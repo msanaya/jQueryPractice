c83b46f085a98104a4713b6b464160d86dd958a5
Pilar-B <pbarkerit@gmail.com>
2021-10-12 18:15:35 -0400

    added bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -34,4 +34,41 @@ jQuery(document).ready(function () {
         $("body").addClass("red-background");
     });
 
+    $("button#green").click(function () {
+        $("body").removeClass();
+        $("body").addClass("green-background");
+    });
+
+    $("button#yellow").click(function () {
+        $("body").removeClass();
+        $("body").addClass("yellow-background");
+    });
+
+    $("button#red").click(function () {
+        $("body").removeClass();
+        $("body").addClass("red-background");
+    });
+
+    $("button#hello").click(function () {
+        $("ul#user").prepend("<li>Hello!</li>");
+        $("ul#webpage").prepend("<li>Why hello there!</li>");
+        $("ul#user").children("li").first().click(function () {
+            $(this).remove();
+        });
+        $("ul#webpage").children("li").first().click(function () {
+            $(this).remove();
+        });
+    });
+
+    $("button#goodbye").click(function () {
+        $("ul#user").prepend("<li>Goodbye!</li>");
+        $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    });
+
+    $("button#stop").click(function () {
+        $("ul#user").prepend("<li>Stop copying me!</li>");
+        $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    });
+
+
 });
