3ecf6000941931d10220d331ec791515dda80829
Pilar-B <pbarkerit@gmail.com>
2021-10-14 19:32:35 -0400

    Added new html file, car insurance practice

--- js/scripts.js
+++ js/scripts.js
@@ -70,5 +70,16 @@ jQuery(document).ready(function () {
         $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
     });
 
+    $("form#insurance").submit(function (event) {
+        const age = parseInt($("input#age").val());
+        const gender = $("select#gender").val();
 
+        const quote = (100 - age) * 3;
+
+        $("#rate").text(quote);
+        $("#quote").show();
+
+        event.preventDefault();
+
+    });
 });
